60e77f9fc6d08c89ac49d50936230d8867614c53
dev-ia2k24 <dev_ia@outlook.com>
2024-08-28 20:59:13 +0000

    primer commit

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,21 @@
 // Placeholder to host a component
+import React from 'react';
+const Hours = () =>{
+const currentDay=new Date().getDay();
+let openingHours;
+if(currentDay>=1 && currentDay<=5)
+{openingHours='10am-4pm';}
+else if(currentDay===0 || currentDay===6){
+    openingHours='9am-8pm';
+    }
+    else{
+        openingHours='Closed-Cerrado'
+    }
+return(
+<div>
+    <h2>Shelter Hours- Horario</h2>
+    <p>Today's Hours - Horario del día:{openingHours}</p>
+</div>
+);
+};
+export default Hours;
